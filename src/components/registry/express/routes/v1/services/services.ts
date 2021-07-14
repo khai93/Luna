8f94fb24297bb80b0d21108e4f9f8ff90c632c9b
khai93 <33293519+khai93@users.noreply.github.com>
2021-07-14 16:53:04 -0500

    add basic ejs page

--- src/components/registry/express/routes/v1/services/services.ts
+++ src/components/registry/express/routes/v1/services/services.ts
@@ -8,6 +8,8 @@ import { TOKENS } from "src/di";
 import { ServiceModule } from "src/modules/service/types";
 import { inject, autoInjectable } from "tsyringe";
 import Version from "../../../../../../common/version";
+import { IKeyValuePair } from "src/common/interfaces/IKeyValuePair";
+import { InstanceRaw } from "src/common/instance/instance";
 
 
 @autoInjectable()
@@ -20,10 +22,21 @@ export class ExpressRegistryServicesRoute implements IExpressRoute {
 
     execute(router: Router) {
         router.get("/services", catchErrorAsync(async (req, res) => {
-            const servicesData = await this.serviceModule!.getAll();
-            const rawData = servicesData.map(service => service.raw);
+            const instances = await this.serviceModule!.getAll();
 
-            res.send(rawData);
+            const instancesByServiceName = instances.reduce<IKeyValuePair<InstanceRaw[]>>((acc, curr) => {
+                if (acc[curr.value.name.value] == null) {
+                    acc[curr.value.name.value] = [];
+                }
+
+                acc[curr.value.name.value].push(curr.raw);
+
+                return acc;
+            }, {});
+
+            console.log(instancesByServiceName);
+            
+            res.render("services", { services: instancesByServiceName });
         }));
     };
 }
