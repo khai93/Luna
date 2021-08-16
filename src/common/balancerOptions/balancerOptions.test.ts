39d7a6035923399d5d40da47d2540cc44f34111c
khai93 <33293519+khai93@users.noreply.github.com>
2021-08-16 03:27:24 -0500

    fix balancerOption valid test

--- src/common/balancerOptions/balancerOptions.test.ts
+++ src/common/balancerOptions/balancerOptions.test.ts
@@ -8,10 +8,6 @@ import { BalancerOptions, BalancerOptionsNotValidError } from "./balancerOptions
 
 describe("BalancerOptions class", () => {
     it("should throw NotValidError if passed in a non-number as weight", () => {
-        try {
-            const n = new BalancerOptions({weight: "" as any});
-        } catch (e) {
-            expect(e).toThrowError(BalancerOptionsNotValidError)
-        }
+        expect( new BalancerOptions({weight: "" as any})).toThrowError(BalancerOptionsNotValidError)
     })
 })
